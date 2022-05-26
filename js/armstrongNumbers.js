bd4a75a3bfbb50612a4125235625f7cc9f9d367b
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-26 12:18:28 -0700

    armstrong solve js

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,22 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
+exports.findArmstrongNumbers = function(input) {
 
+    let result = [];
+    let numString;
+    let answer;
+
+    for (let num = 1; num <= input; num++) {
+        answer =0;
+        numString = num.toString().split('');
+        expo = numString.length;
+        for (let i =0; i< numString.length; i++) {
+            numString[i] = (numString[i] * 1) ** expo;
+            answer += numString[i] ;
+        if (answer === num) {
+            result.push(answer);
+            }
+        }
+    }
+    return result;
 };
